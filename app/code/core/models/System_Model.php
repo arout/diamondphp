50eb39a67ce70bb5bccb2d45f0dabb22599ceb24
Andrew Rout <andrew_rout@yahoo.com>
2016-07-05 20:45:29 -0400

    Code cleanup

--- app/code/core/models/System_Model.php
+++ app/code/core/models/System_Model.php
@@ -2,56 +2,56 @@
 namespace Hal\Model;
 ob_start();
 
-class System_Model {
+class System_Model 
+{	
+	public 		$cache;
+	protected 	$config;
+	public 		$data;
+	protected 	$db;
+	public 		$errors = [];
+	public 		$hash;
+	public 		$load;
+	public 		$log;
+	public 		$session;
+	protected 	$toolbox;
 	
-	protected $db;
-	protected $config;
-	// Sessions
-	public $session;
-	// Data caching helper
-	public $cache;
-	// Data accessed by views / controllers
-	public $data;
-	public $hash;
-	public $load;
-	protected $toolbox;
-	
-	public function __construct( $app ) {
-	
-		$this->db           = $app['database'];
-		$this->config       = $app['config'];
-		//$this->load       = $load;
-		$this->toolbox 	    = $app['toolbox'];
-		$this->session      = self::session();
-		//$this->hash         = self::hash();
+	public function __construct( $app ) 
+	{
 		$this->cache        = self::cache();
+		$this->config       = $app['config'];
+		$this->db           = $app['database'];
+		$this->log          = $app['log'];
+		$this->session      = self::session();
+		$this->toolbox 	    = $app['toolbox'];
+		//$this->load       = $load;
 	}
 	
-	public function cache() {
+	public function cache() 
+	{
 		// return \Application::run('Cache');
 	}
 
-	public function encrypt( $string ) {
-
+	public function encrypt( $string ) 
+	{
 		# Encrypt using password_hash()
 		$hash = new \Hal\Module\Hash;
 		return $hash->encrypt($string);
 	}
 	
-	public function verify($string, $base) {
-        
+	public function verify($string, $base) 
+	{
         # Decrypt hash from encrypt() 
         $hash = new \Hal\Module\Hash;
 		return $hash->verify($string, $base);
 	}
 
-	public function session() {
-
+	public function session() 
+	{
 		return $this->toolbox["session"];
 	}
 
-	public function toolbox($helper) {
-
+	public function toolbox($helper) 
+	{
 		# Load a Toolbox helper
 		return $this->toolbox["$helper"];
 	}
