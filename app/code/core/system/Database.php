7facd7c234ebdb40696a7783f67b954c9197a892
Andrew Rout <andrew_rout@yahoo.com>
2016-12-08 04:55:42 -0500

    Cleanup core database class

--- app/code/core/system/Database.php
+++ app/code/core/system/Database.php
@@ -2,30 +2,14 @@
 namespace Hal\Core;
 use \PDO as PDO;
 
-/*
- * working example
- *
- * $query = $_application['database']->sql->prepare('SELECT * FROM users');
- * $query->execute();
- *
- * foreach ($query as $u)
- *	echo $u['username'];
- *
- */
 class Database extends PDO
 {
-
 	public $sql;
 
-	public function __construct($config)
+	public function __construct($c)
 	{
-		parent::__construct("mysql:host=" . $config->setting('db_host') . ";dbname=" . $config->setting('db_name') . "", $config->setting('db_user'), $config->setting('db_pass'));
+		parent::__construct("mysql:host=" . $c['config']->setting('db_host') . ";dbname=" . $c['config']->setting('db_name') . "", $c['config']->setting('db_user'), $c['config']->setting('db_pass'));
 		PDO::setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		PDO::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-
-	public function run()
-	{
-		return;
 	}
 }
