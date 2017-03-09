358b9d532b227d34307d58e208bf81d172c70033
Andrew Rout <andrew_rout@yahoo.com>
2017-03-09 14:41:21 -0500

    Removed strict type references (https://github.com/arout/diamondphp/issues/4)  and added vendor directory to .gitignore (https://github.com/arout/diamondphp/issues/2)

--- app/code/core/system/global.php
+++ app/code/core/system/global.php
@@ -2,9 +2,6 @@
 
 ### Define global server / PHP settings ###
 
-# Strict mode by default for scalar type declarations
-declare(strict_types = 1);
-
 # Get Composer autoloader
 require_once BASE_PATH . 'vendor/autoload.php';
 require_once 'factory.php';
@@ -14,11 +11,11 @@ date_default_timezone_set($app['config']->setting('time_zone'));
 
 # Turn error reporting on / off. Do not adjust error reporting here,
 # do so from /app/code/core/config/Config.php
-$app['parse']->set_reporting( $app['config']->setting('error_reports') );
+$app['parse']->set_reporting($app['config']->setting('error_reports'));
 
 # Log run time errors
-if( $app['config']->setting('log_errors') === TRUE ) 
+if ($app['config']->setting('log_errors') === TRUE)
 {
 	ini_set("log_errors", '1');
-	ini_set("error_log", LOG_PATH.'system.log');
+	ini_set("error_log", LOG_PATH . 'system.log');
 }
