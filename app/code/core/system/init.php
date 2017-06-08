72a8a3eea58e5fbf993ef4c74bcefd563c4b2788
Andrew Rout <andrew@diamondphp.com>
2017-06-08 19:07:45 -0400

    Add license info to front controller; system intialization tweaks

--- app/code/core/system/init.php
+++ app/code/core/system/init.php
@@ -1,10 +1,19 @@
 <?php
 
-$file = BASE_PATH.'.env';
-if (!file_exists($file)) {
+$file = BASE_PATH . '.env';
+if (!file_exists($file))
+{
 	exit('<h3><span style="color: red;">.env</span> global configuration file not found. Exiting...</h3>');
 }
 
-require_once BASE_PATH.'vendor/autoload.php';
-require_once BASE_PATH.'app/code/core/system/factory.php';
-require_once BASE_PATH.'app/code/core/system/paths.php';
+require_once BASE_PATH . 'vendor/autoload.php';
+require_once BASE_PATH . 'app/code/core/system/factory.php';
+require_once BASE_PATH . 'app/code/core/system/paths.php';
+
+# Core Smarty settings
+$app['template']->setTemplateDir(VIEWS_PATH);
+$app['template']->setCompileDir(VAR_PATH . 'templates_c');
+$app['template']->setCacheDir(CACHE_PATH . '/' . $app['router']->controller_class . '/' . $app['router']->action . '/' . $app['router']->param1);
+$app['template']->setCaching(Smarty::CACHING_LIFETIME_CURRENT);
+#$app['template']->setCompileCheck(false);
+$app['template']->setConfigDir(SMARTY_PATH . 'configs');
--- app/code/core/system/run.php
+++ app/code/core/system/run.php
@@ -8,20 +8,23 @@ $_page_exec_timer_start = microtime(true);
 # Actual memory used
 // $_ram = memory_get_usage ( false );
 # Format memory use to kb or mb
-function convert($_ram) {
+function convert($_ram)
+{
 	$unit = ['b', 'kb', 'mb', 'gb', 'tb', 'pb'];
-	return round($_ram/pow(1024, ($i = floor(log($_ram, 1024)))), 2).' '.$unit[$i];
+	return round($_ram / pow(1024, ($i = floor(log($_ram, 1024)))), 2) . ' ' . $unit[$i];
 }
 $actual_ram = convert(memory_get_usage(false));
 
-require_once SYSTEM_PATH.'factory.php';
+require_once SYSTEM_PATH . 'factory.php';
 
-if ($app['config']->setting('maintenance_mode') === "TRUE") {
-	header('Location: '.$app['config']->setting('site_url').'maintenance.php');
+if ($app['config']->setting('maintenance_mode') === "TRUE")
+{
+	header('Location: ' . $app['config']->setting('site_url') . 'maintenance.php');
 	exit;
 }
 
-if ($app['config']->setting('system_startup_check') === "TRUE") {
+if ($app['config']->setting('system_startup_check') === "TRUE")
+{
 	require_once 'system_startup_check.php';
 	exit;
 }
@@ -29,26 +32,25 @@ if ($app['config']->setting('system_startup_check') === "TRUE") {
 # Build system routes
 $app['router']->build();
 
-if ($app['session']->get('admin_username')) {
+if ($app['session']->get('admin_username'))
+{
 	$nav_menu = 'nav_user.tpl';
-} elseif ($app['session']->get('username')) {
+}
+elseif ($app['session']->get('username'))
+{
 	$nav_menu = 'nav_user.tpl';
-} else {
+}
+else
+{
 	$nav_menu = 'nav_visitor.tpl';
 }
 $app['template']->assign('slogan', $app['config']->setting('site_slogan'));
-# Core Smarty settings
-$app['template']->setTemplateDir(VIEWS_PATH);
-$app['template']->setCompileDir(VAR_PATH.'templates_c');
-$app['template']->setCacheDir(CACHE_PATH.'/'.$app['router']->controller_class.'/'.$app['router']->action.'/'.$app['router']->param1);
-$app['template']->setCaching(Smarty::CACHING_LIFETIME_CURRENT);
-#$app['template']->setCompileCheck(false);
-$app['template']->setConfigDir(SMARTY_PATH.'configs');
 
 # Memory usage
 $app['template']->assign('actual_ram', $actual_ram);
 
-if ($app['router']->controller_class === $app['config']->setting['admin_controller'].'_Controller') {
+if ($app['router']->controller_class === $app['config']->setting['admin_controller'] . '_Controller')
+{
 	$app['template']->setTemplateDir(ADMIN_VIEWS_PATH);
 	// Set the page titles in head.tpl
 	$app['template']->assign('page_title', $app['title']->get());
@@ -60,17 +62,22 @@ if ($app['router']->controller_class === $app['config']->setting['admin_controll
 	$app['base_controller']->parse();
 
 	# Stop timer for page execution
-	$_page_exec_timer_stop = (microtime(true)-$_page_exec_timer_start);
+	$_page_exec_timer_stop = (microtime(true) - $_page_exec_timer_start);
 	$app['template']->assign('script_exec_time', $_page_exec_timer_stop);
 
 	$app['template']->display('layout.tpl');
-	if ($app['router']->action !== 'login') {
+	if ($app['router']->action !== 'login')
+	{
 		$app['template']->display('footer.tpl');
-	} else {
+	}
+	else
+	{
 		$app['template']->display('footer-login.tpl');
 	}
 
-} else {
+}
+else
+{
 	// Set the page titles in head.tpl
 	$app['template']->assign('page_title', $app['title']->get());
 	// Get appropriate nav menu
@@ -82,10 +89,11 @@ if ($app['router']->controller_class === $app['config']->setting['admin_controll
 	$app['base_controller']->parse();
 
 	# Stop timer for page execution
-	$_page_exec_timer_stop = (microtime(true)-$_page_exec_timer_start);
+	$_page_exec_timer_stop = (microtime(true) - $_page_exec_timer_start);
 	$app['template']->assign('script_exec_time', $_page_exec_timer_stop);
 
-	if ($app['router']->controller_class !== 'Block_Controller') {
+	if ($app['router']->controller_class !== 'Block_Controller')
+	{
 		$app['template']->display('extends:layout.tpl|head.tpl');
 	}
 
